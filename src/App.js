d3bd6ad7d3025697771dc96f5a33a96f66d9db90
Brandon Combs <brandon@combscs.com>
2021-12-12 15:35:12 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home';
 import About from './components/about';
 
 function App() {
   return (
     <>
-      <Router>
+      <Router base="/">
       <Routes>
         <Route path='/' exact element={<Home />} />
         <Route path='/about' exact element={<About />} />
