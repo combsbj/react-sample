f0c6817274c93376d9385f77dfbdb0dfab80e488
Brandon Combs <brandon@combscs.com>
2021-12-12 14:37:25 -0500

    test

---
+++ src/components/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+    <h1>About</h1>
+    <a href='/'>Home</a>
+    </>
+  );
+}
+
+export default About;
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+    <h1>Home</h1>
+    <a href='/about'>About</a>
+    </>
+  );
+}
+
+export default Home;
